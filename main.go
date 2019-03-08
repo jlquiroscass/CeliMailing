caac0fd994775d006498e51f588cbef71be9bf7b
Juan Luis Quirós <jl.quiros.cassillas@gmail.com>
2019-03-08 18:06:06 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
+	"github.com/gorilla/mux"
+)
+
+const (
+	// Replace sender@example.com with your "From" address.
+	// This address must be verified with Amazon SES.
+	Sender = "comunicacion@celicidad.net"
+	//The email body for recipients with non-HTML email clients.
+	TextBody = "This email was sent with Amazon SES using the AWS SDK for Go."
+
+	// The character encoding for the email.
+	CharSet = "UTF-8"
+)
+
+// Mail The mail Type (more like an object)
+type Mail struct {
+	Subject string `json:"subject,omitempty"`
+	To      string `json:"to,omitempty"`
+	Body    string `json:"body,omitempty"`
+}
+
+// send mail
+func sendMail(w http.ResponseWriter, r *http.Request) {
+	var mail Mail
+	_ = json.NewDecoder(r.Body).Decode(&mail)
+	log.Print("Sending mail to " + mail.To)
+	var Recipient = mail.To
+	var Subject = mail.Subject
+	var HtmlBody = mail.Body
+	// Create a new session in the eu-west-1 region.
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+
+	// Create an SES session.
+	svc := ses.New(sess)
+
+	// Assemble the email.
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(Recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(HtmlBody),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(CharSet),
+					Data:    aws.String(TextBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(CharSet),
+				Data:    aws.String(Subject),
+			},
+		},
+		Source: aws.String(Sender),
+	}
+
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
+
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+
+		return
+	}
+
+	fmt.Println("Email Sent to address: " + Recipient)
+	fmt.Println(result)
+	json.NewEncoder(w).Encode(mail)
+}
+
+// main function to boot up everything
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/mail", sendMail).Methods("POST")
+	log.Fatal(http.ListenAndServe(":6565", router))
+}
